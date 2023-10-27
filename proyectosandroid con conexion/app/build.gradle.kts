70474a95cbad82da3534365ac302bdac515ddc0f
MartinCorral883 <cmc_883@hotmail.com>
2023-10-27 01:28:21 -0300

    subo items y holder sin alcohol

--- proyectosandroid con conexion/app/build.gradle.kts
+++ proyectosandroid con conexion/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.asd"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.asd"
@@ -31,19 +31,21 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.airbnb.android:lottie:3.0.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation ("com.facebook.fresco:fresco:2.5.0")
-    implementation ("io.coil-kt:coil:1.6.0")
+
+
+
 
 
 }
