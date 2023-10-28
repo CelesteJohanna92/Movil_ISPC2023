cac954d550d9ca1a0710fdda580e2aea38baa841
viktoryia40 <ozarenko2719@gmail.com>
2023-10-28 16:51:55 -0300

    Modifico archivos sin alcohol

--- proyectosandroid con conexion/app/src/main/java/com/example/asd/SinAlcohol.java
+++ proyectosandroid con conexion/app/src/main/java/com/example/asd/SinAlcohol.java
@@ -1,21 +1,24 @@
-package com.example.asd.SinAlcohol;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+package com.example.asd;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.asd.R;
+
+import java.util.List;
 
 public class SinAlcohol extends AppCompatActivity {
 
     RecyclerView recyclerViewSinAlcohol;
+    RecetaAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +26,20 @@ public class SinAlcohol extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
-        actionBar.setTitle("Categorias");
+        actionBar.setTitle("Categoría Sin Alcohol");
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+
+        recyclerViewSinAlcohol = findViewById(R.id.recyclerViewSinAlcohol);
+        recyclerViewSinAlcohol.setLayoutManager(new LinearLayoutManager(this));
+
+        daoUsuario dao = new daoUsuario(this);
+        List<Receta> listaRecetasSinAlcohol = dao.selectRecetasSinAlcohol();
+        dao.close();
+
+        adapter = new RecetaAdapter(listaRecetasSinAlcohol, this);
+        recyclerViewSinAlcohol.setAdapter(adapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,7 +61,6 @@ public class SinAlcohol extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
     @Override
     public boolean onSupportNavigateUp() {
